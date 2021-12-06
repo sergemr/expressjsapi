b0af696c7dfb6bb55f2e03e161bdb1c44159ea29
samor737 <sergio@appartisans.it>
2021-12-06 08:47:07 -0800

    fix: refactoring

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const app = express();
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 const fs = require("fs");
-var cors = require('cors');
+const cors = require('cors');
 const { title } = require("process");
-var Logger = (exports.Logger = {});
-var infoStream = fs.createWriteStream("logs/info.txt");
+const Logger = (exports.Logger = {});
+const infoStream = fs.createWriteStream("logs/info.txt");
 app.use(cors());
 app.options('*', cors());
 app.get('/tasks', cors (), function (req, res, next) {
-	var number = req.query.number || 3;
-	var result = axios.get('https://lorem-faker.vercel.app/api?quantity='+number)
+	const number = req.query.number || 3;
+	axios.get('https://lorem-faker.vercel.app/api?quantity='+number)
 	  .then(response => {
 		  console.log(response);
 		  let dataArr = [];
@@ -34,7 +34,7 @@ app.get('/tasks', cors (), function (req, res, next) {
 //Log File Entry
 //Log Location /logs/info.txt
 app.put('/tasks',express.json({type: 'application/json'}), function(request, response) {
-	var task = request.body;
+	const task = request.body;
 	infoStream.write("id: "+task.id+" title: "+task.title+"\n");
     response.send({value:'this is an update '+task.id});
 	console.log("id: "+task.id+" title: "+task.title);
